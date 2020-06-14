bbcaad2a6abd359205380da3e6aaf84769f1c190
ManavR123 <manav.rathod@berkeley.edu>
2020-06-14 17:21:10 -0400

    moved and finished test.py

--- server/test.py
+++ server/test.py
@@ -1,15 +1,18 @@
-from UnigramModel import UnigramModel
-from NGramModel import NGramModel
-from DiscountBackoffModel import DiscountBackoffModel
-from KneserNeyBaseModel import KneserNeyBaseModel
-from NeuralNGramModel import NeuralNGramModel
-from LSTMModel import LSTMModel
+import os
+import pickle
 
-from utils import (
+from models.utils import (
     check_validity,
     generate_text,
     save_truncated_distribution,
 )
+from models.UnigramModel import UnigramModel
+from models.NGramModel import NGramModel
+from models.DiscountBackoffModel import DiscountBackoffModel
+from models.KneserNeyBaseModel import KneserNeyBaseModel
+from models.NeuralNGramModel import NeuralNGramModel
+from models.LSTMModel import LSTMModel
+
 import torchtext
 
 text_field = torchtext.data.Field()
@@ -21,38 +24,51 @@ vocab = text_field.vocab
 train_text = train_dataset.examples[0].text  # a list of tokens (strings)
 validation_text = validation_dataset.examples[0].text
 
-unigram_demonstration_model = UnigramModel(train_text, vocab)
-print(
-    "unigram validation perplexity:",
-    unigram_demonstration_model.perplexity(validation_text),
-)
-check_validity(unigram_demonstration_model, validation_text)
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, "saved_models/")
 
-unigram_model = NGramModel(train_text, vocab, 1)
+unigram_model = NGramModel(train_text, vocab, n=1)
 check_validity(unigram_model, validation_text)
 print("unigram validation perplexity:", unigram_model.perplexity(validation_text))
+with open(filename + "unigram.pkl", "wb") as f:
+    pickle.dump(unigram_model, f)
+print("SAVED: UNIGRAM MODEL")
+
 bigram_model = NGramModel(train_text, vocab, n=2)
 check_validity(bigram_model, validation_text)
 print("bigram validation perplexity:", bigram_model.perplexity(validation_text))
+with open(filename + "bigram.pkl", "wb") as f:
+    pickle.dump(bigram_model, f)
+print("SAVED: BIGRAM MODEL")
+
 
 trigram_model = NGramModel(train_text, vocab, n=3)
 check_validity(trigram_model, validation_text)
 print("trigram validation perplexity:", trigram_model.perplexity(validation_text))
+with open(filename + "trigram.pkl", "wb") as f:
+    pickle.dump(trigram_model, f)
+print("SAVED: TRIGRAM MODEL")
 
-save_truncated_distribution(bigram_model, "predictions/bigram_predictions.npy")
 
 bigram_backoff_model = DiscountBackoffModel(train_text, vocab, unigram_model, 2)
 check_validity(bigram_backoff_model, validation_text)
+
 trigram_backoff_model = DiscountBackoffModel(train_text, vocab, bigram_backoff_model, 3)
 check_validity(trigram_backoff_model, validation_text)
 print(
     "trigram backoff validation perplexity:",
     trigram_backoff_model.perplexity(validation_text),
 )
+with open(filename + "trigram_backoff.pkl", "wb") as f:
+    pickle.dump(trigram_backoff_model, f)
+print("SAVED: TRIGRAM_BACKOFF MODEL")
+
 
 kn_base = KneserNeyBaseModel(train_text, vocab)
 check_validity(kn_base, validation_text)
+
 bigram_kn_backoff_model = DiscountBackoffModel(train_text, vocab, kn_base, 2)
+
 trigram_kn_backoff_model = DiscountBackoffModel(
     train_text, vocab, bigram_kn_backoff_model, 3
 )
@@ -60,7 +76,9 @@ print(
     "trigram Kneser-Ney backoff validation perplexity:",
     trigram_kn_backoff_model.perplexity(validation_text),
 )
-save_truncated_distribution(trigram_kn_backoff_model, "predictions/trigram_kn_predictions.npy")
+with open(filename + "trigram_kn_backoff.pkl", "wb") as f:
+    pickle.dump(trigram_kn_backoff_model, f)
+print("SAVED: TRIGRAM_KN_BACKOFF MODEL")
 
 neural_trigram_model = NeuralNGramModel(3, train_text, validation_text, vocab)
 check_validity(neural_trigram_model, validation_text)
@@ -69,10 +87,13 @@ print(
     "neural trigram validation perplexity:",
     neural_trigram_model.perplexity(validation_text),
 )
+with open(filename+"neural_trigram.pkl", "wb") as f:
+    pickle.dump(neural_trigram_model, f)
+print("SAVED: NEURAL_TRIGRAM MODEL")
 
-save_truncated_distribution(neural_trigram_model, "predictions/neural_trigram_predictions.npy")
-
-lstm_model = LSTMModel(train_text, vocab)
-lstm_model.train()
-print("lstm validation perplexity:", lstm_model.dataset_perplexity(validation_dataset))
-save_truncated_distribution(lstm_model, "predictions/lstm_predictions.npy")
+LSTM = LSTMModel(train_dataset, validation_dataset, vocab)
+LSTM.train()
+print("lstm validation perplexity:", LSTM.dataset_perplexity(validation_dataset))
+with open(filename+"LSTM.pkl", "wb") as f:
+    pickle.dump(LSTM, f)
+print("SAVED: LSTM MODEL")
